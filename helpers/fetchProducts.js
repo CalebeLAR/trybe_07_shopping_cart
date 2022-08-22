d733c0c1da93ffde107b2415402141652c748908
CalebeLAR <ca6ele@gmail.com>
2022-08-22 00:12:55 -0400

    requisito 1 testado e concluído

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,5 +1,11 @@
-const fetchProducts = () => {
-  // seu código aqui
+const fetchProducts = async (searchedProduct) => {
+  if (searchedProduct) {
+    const url = `https://api.mercadolibre.com/sites/MLB/search?q=${searchedProduct}`;
+    const response = await fetch(url);
+    const productList = await response.json();
+    return productList;
+  } 
+  throw new Error('You must provide an url');
 };
 
 if (typeof module !== 'undefined') {
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -3,6 +3,31 @@ const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  test('verifica se fetchProducts é uma função.', () => {
+    expect(typeof fetchProducts).toBe('function');
+  });
+
+  test('verifica se fetchProducts ao receber o argumento \'computador\' chama a função fetch.', async ()=>{
+    expect.assertions(1);
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalled();
+  });
+
+  test('verifica se fetchProducts ao receber o argumento \'computador\' chama a função fetch com o endpoint \'https://api.mercadolibre.com/sites/MLB/search?q=computador\'.', async ()=>{
+    expect.assertions(1);
+    const url = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalledWith(url);
+  });
+
+  test('verifica se fetchProducts ao receber o argumento \'computador\' retorna um objeto similar ao armazenado em \'item\'.', async ()=>{
+    expect.assertions(1);
+    const result = await fetchProducts('computador');
+    expect(result).toStrictEqual(computadorSearch); // testar se os objetos têm os mesmos tipos e estrutura.
+  });
+
+  test('verifica se ao chamar fetchProducts sem argumento, ela retorna um erro com a mensagem: \'You must provide an url\'', async ()=>{
+    expect.assertions(1);
+    await expect(fetchProducts()).rejects.toThrow('You must provide an url');
+    });
 });
